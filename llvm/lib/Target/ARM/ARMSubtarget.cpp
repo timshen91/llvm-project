timshen91/llvm-project

commit 9ee3253542f7ff51f228958a1683a20b1316d952

Replace string matching with a switch on Triple::getEnvironment.
llvm-svn: 197332

llvm/lib/Target/ARM/ARMSubtarget.cpp
@@ -189,11 +189,17 @@ void ARMSubtarget::resetSubtargetFeatures(StringRef CPU, StringRef FS) {
   // Initialize scheduling itinerary for the specified CPU.
   InstrItins = getInstrItineraryForCPU(CPUString);
 
-  if ((TargetTriple.getTriple().find("eabi") != std::string::npos) ||
-      (isTargetIOS() && isMClass()))
-    // FIXME: We might want to separate AAPCS and EABI. Some systems, e.g.
-    // Darwin-EABI conforms to AACPS but not the rest of EABI.
+  switch (TargetTriple.getEnvironment()) {
+  case Triple::EABI:
+  case Triple::GNUEABI:
+  case Triple::GNUEABIHF:
     TargetABI = ARM_ABI_AAPCS;
+    break;
+  default:
+    if (isTargetIOS() && isMClass())
+      TargetABI = ARM_ABI_AAPCS;
+    break;
+  }
 
   if (isAAPCS_ABI())
     stackAlignment = 8;

commit 0956a38e24c45659757b102808e50adbe8d48353

Fix Android regression in r197332.

llvm/lib/Target/ARM/ARMSubtarget.cpp
@@ -190,6 +190,7 @@ void ARMSubtarget::resetSubtargetFeatures(StringRef CPU, StringRef FS) {
   InstrItins = getInstrItineraryForCPU(CPUString);
 
   switch (TargetTriple.getEnvironment()) {
+  case Triple::Android:
   case Triple::EABI:
   case Triple::GNUEABI:
   case Triple::GNUEABIHF: